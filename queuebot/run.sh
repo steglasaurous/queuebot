c7f65df380705cc7e33065936eb8f763d6d78703
Scott Teglasi <scott.teglasi@experiencepoint.com>
2023-09-10 13:14:25 -0400

    Move env vars to their proper places.

--- queuebot/run.sh
+++ queuebot/run.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
+# FIXME: Add .env params configured here
+# FIXME: Add twitch_token.json to a docker volume so it survives upgrades.
 cd /server && npm run typeorm:run-migrations && npm run start
