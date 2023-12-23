2a3786d716a087d309bf4b1cc9d10043e8470be5
Steglasaurous <me@steglasaurous.com>
2023-12-23 12:58:53 -0500

    Add auto-downloader for spin rhythm

--- streamerbot-actions/requestobot-connect.cs
+++ streamerbot-actions/requestobot-connect.cs
@@ -4,8 +4,9 @@ public class CPHInline
 {
 	public bool Execute()
 	{
+		CPH.LogDebug("Executing connect thing");
 		// We emit a subscribe message to the websocket server.
-		CPH.WebsocketSend("{ \"event\": \"subscribe\", \"data\": { \"channelName\": \"" + args["channelName"].ToString() + "\" } }", 0);
+		CPH.WebsocketSend("{ \"event\": \"subscribe\", \"data\": { \"channelName\": \"" + args["channelName"].ToString() + "\" } }", 5);
 
 		return true;
 	}
