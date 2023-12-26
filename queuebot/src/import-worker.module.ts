32d0bd5f3191e0729c0a9e862618306b11f35ba0
Steglasaurous <me@steglasaurous.com>
2023-12-26 16:23:24 -0500

    Moved importer to its own worker

---
+++ queuebot/src/import-worker.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { DataStoreModule } from './modules/data-store/data-store.module';
+import { SongStoreModule } from './modules/song-store/song-store.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeORMAppConfig } from './typeorm.config';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    DataStoreModule,
+    SongStoreModule,
+    TypeOrmModule.forRoot({ ...typeORMAppConfig }),
+  ],
+})
+export class ImportWorkerModule {}
