f4f15717d20066b95566a3e2c41fc474ff20322f
Steglasaurous <me@steglasaurous.com>
2024-03-25 20:02:25 -0400

    Remove dev env from client - no longer required.  Add WEBSOCKET_URL environment var

--- queuebot-client/src/environments/environment.development.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  queuebotApiBaseUrl: 'http://localhost:3000',
-  jwtPublicKey: 'queueballs', // For development, as long as we're using the same secret it's fine.
-  websocketHost: 'localhost',
-  websocketPort: 3000,
-  websocketPath: '',
-  websocketUrl: 'ws://localhost:3000',
-};
